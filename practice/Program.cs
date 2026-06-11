914a28e6916124ff450d9050241c1a692b4b8f84
Devin Rider <riderdevin@yahoo.com>
2026-06-10 22:50:49 -0700

    Initial Commit, C# Practice Projects

---
+++ practice/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+CONSOLE INPUT / OUTPUT
+
+Printing to the Terminal, using the String kinda like an F string in Python
+
+Console.WriteLine("What plastic is your disc made of?");
+string? plastic = Console.ReadLine();
+
+Console.WriteLine("What Mold is your disc?");
+string? mold = Console.ReadLine();
+
+Console.WriteLine($"Your Disc is a {plastic} {mold}");
+*/
+
+
+
+
+
+/*
+VARIABLES AND TYPES
+
+Declaring variables and using them in printed terminal lines
+
+string name = "explorer";
+string brand = "Lattitude";
+int speed = 7;
+int glide = 5;
+int turn = 0;
+int fade = 2;
+
+Console.WriteLine($"{brand} {name}");
+Console.WriteLine($"Flight: {speed} / {glide} / {turn} / {fade}");
+*/
+
+
+
+
+
