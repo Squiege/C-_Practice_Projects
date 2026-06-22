669889204579801693eaf5b303e701840805bdb2
Devin Rider <riderdevin@yahoo.com>
2026-06-21 21:49:00 -0700

    Notes Update

--- practice/Program.cs
+++ practice/Program.cs
@@ -34,5 +34,124 @@ Console.WriteLine($"Flight: {speed} / {glide} / {turn} / {fade}");
 
 
 
+/* PARSING NUMBERS 
+
+Getting user input then attempting to convert it from a string to a number stored in a new variable through a parse 
+
+Console.WriteLine("Enter your discs speed:");
+string? inputSpeed = Console.ReadLine();
+
+Console.WriteLine($"Enter your discs glide:");
+string? inputGlide = Console.ReadLine();
+
+Console.WriteLine($"Enter your discs turn:");
+string? inputTurn = Console.ReadLine();
+
+Console.WriteLine($"Enter your discs fade:");
+string? inputFade = Console.ReadLine();
+
+bool successSpeed = int.TryParse(inputSpeed, out int speed);
+bool successGlide = int.TryParse(inputGlide, out int glide);
+bool successTurn = int.TryParse(inputTurn, out int turn);
+bool successFade = int.TryParse(inputFade, out int fade);
+
+if (successSpeed)
+{
+    if (successGlide)
+    {
+        if (successTurn)
+        {
+            if (successFade)
+            {
+                Console.WriteLine($"Your discs flight numbers are {speed} / {glide} / {turn} / {fade}");
+            }
+            else
+            {
+                Console.WriteLine($"Please enter a number");
+            }
+
+        }
+        
+    else
+    {
+        Console.WriteLine($"Please enter a number");
+    }
+    }
+}
+else
+{
+    Console.WriteLine($"Please enter a number");
+}
+
+*/
 
 
+
+/* 
+CONDITIONALS 
+
+Pretty much the same syntax as python, make sure to use '==' for equals to
+
+Console.WriteLine($"Enter a speed:");
+string? input = Console.ReadLine();
+
+bool validate = int.TryParse(input, out int speed);
+
+if (speed < 4)
+{
+    Console.WriteLine($"Your chose a putter");
+}
+else if (speed == 4)
+{
+    Console.WriteLine($"Your chose an approach");
+}
+else if (speed == 5)
+{
+    Console.WriteLine($"You chose a midrange");
+}
+else if (speed > 5 && speed < 10)
+{
+    Console.WriteLine($"You chose a Fairway");
+}
+else if (speed >= 10)
+{
+    Console.WriteLine($"You chose a Distance Driver");
+}
+else
+{
+    Console.WriteLine("error");
+}
+
+*/
+
+/*    METHODS      
+
+static void DiscMenu()
+{
+
+    Console.WriteLine("1. Add Disc");
+    Console.WriteLine("2. Remove Disc");
+    Console.WriteLine("3. Exit");
+
+    string? input = Console.ReadLine();
+    bool success = int.TryParse(input, out int choice);
+
+    if (success)
+    {
+        NamedDisc("Googa");
+    }
+}
+
+static void NamedDisc(string name)
+{
+    Console.WriteLine($"The Disc is {name}");
+}
+
+static void main()
+{
+    DiscMenu();
+}
+
+main();
+
+*/
